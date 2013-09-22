23aad87ac7a472becbc0b36a4785be9f7aee85d0
Philippe Marchesseault <pmarches@gmail.com>
2013-09-22 00:02:11 -0700

    Account constants, created Accon bean

---
+++ src/jrippleapi/beans/Account.java
@@ -0,0 +1,14 @@
+package jrippleapi.beans;
+
+public class Account {
+	public static final String RIPPLE_ROOT_ACCOUNT="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh";
+	public static final String RIPPLE_ADDRESS_ZERO="rrrrrrrrrrrrrrrrrrrrrhoLvTp";
+	public static final String RIPPLE_ADDRESS_ONE="rrrrrrrrrrrrrrrrrrrrBZbvji";
+	public static final String RIPPLE_ADDRESS_NEUTRAL=RIPPLE_ADDRESS_ONE;
+	public static final String RIPPLE_ADDRESS_NAN="rrrrrrrrrrrrrrrrrrrn5RM1rHd";
+	
+	public static final String RIPPLE_ADDRESS_BITSTAMP = "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B";
+	public static final String RIPPLE_ADDRESS_JRIPPLEAPI="r32fLio1qkmYqFFYkwdnsaVN7cxBwkW4cT";
+	public static final String RIPPLE_ADDRESS_PMARCHES="rEQQNvhuLt1KTYmDWmw12mPvmJD4KCtxmS";
+	
+}
